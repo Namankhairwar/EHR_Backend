4ab6eabfd7331ef4adb3581b0698d67dbb8527fa
Naman khairwar <namankhairwar7@gmail.com>
2026-03-19 20:34:38 +0530

    changes in webconfig

--- src/main/java/com/clinic/patient/config/WebConfig.java
+++ src/main/java/com/clinic/patient/config/WebConfig.java
@@ -13,8 +13,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-//                        .allowedOrigins("http://localhost:3000") // React dev server
-                        .allowedOrigins("https://donedoc.netlify.app") // React dev server
+                        .allowedOrigins("http://localhost:3000") // React dev server
+//                        .allowedOrigins("https://donedoc.netlify.app") // React dev server
 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
