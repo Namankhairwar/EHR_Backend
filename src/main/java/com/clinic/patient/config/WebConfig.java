fd32dc295357d3bb497ad0503b937ffd6041afb3
Naman khairwar <namankhairwar7@gmail.com>
2026-03-19 20:00:22 +0530

    fix

--- src/main/java/com/clinic/patient/config/WebConfig.java
+++ src/main/java/com/clinic/patient/config/WebConfig.java
@@ -13,7 +13,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000") // React dev server
+//                        .allowedOrigins("http://localhost:3000") // React dev server
+                        .allowedOrigins("https://donedoc.netlify.app") // React dev server
+
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
