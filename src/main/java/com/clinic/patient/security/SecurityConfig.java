a6fade997c2a876cf3bcb5d1ec1702779bdc7c59
Krishna <1032241054.@tcetmumbai.in>
2026-06-18 11:09:01 +0530

    cors url added

--- src/main/java/com/clinic/patient/security/SecurityConfig.java
+++ src/main/java/com/clinic/patient/security/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("https://ehr-frontend-new.vercel.app"));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
